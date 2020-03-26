e20ba01ec56d42678b6b42a6ad789c28358c6bbc
hugo <hugosimao@ipvc.pt>
2020-03-26 00:29:30 +0000

    HugoSimao-20752
    Aula5

---
+++ Aula 5/PongGame-HugoSimao-20752/Assets/Scripts/Paddle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+
+{
+
+    public float speed;
+    public Rigidbody2D rb;
+
+    private Vector3 StartPosition;
+
+    private float movement;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPosition = transform.position; // guarda a primeira posição
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Vertical"); //default, W e S como teclas para o Paddle ir para cima e para baixo
+        rb.velocity = new Vector2(rb.velocity.x, movement * speed); // define a velocidade do Paddle
+    }
+
+    public void Reset()
+    {
+        rb.velocity = Vector2.zero; //reset na velocidade
+        transform.position = StartPosition; // reset de velocidade para a inicial
+    }
+}
